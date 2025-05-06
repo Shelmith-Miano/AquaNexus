0458a0cdf33a7ee13f04567d104da0a068122795
shelmith miano <mianoshelmith66@gmail.com>
2025-05-06 11:25:09 +0300

    initial commit, added tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1 className = "text-pink-950">Welcome to React</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+export default App;
